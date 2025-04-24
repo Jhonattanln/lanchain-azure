45a0826d5b574494ffde48c76158cb93e1b678a0
Jhonattan <jhonattanlino@outlook.com>
2025-04-24 16:06:36 -0300

    feat: funcion tools

--- src/agents/clipping_agent.py
+++ src/agents/clipping_agent.py
@@ -8,6 +8,7 @@ from langgraph.graph import MessagesState
 from langchain_core.messages import HumanMessage, SystemMessage
 from langgraph.graph import START, StateGraph
 from langgraph.prebuilt import tools_condition, ToolNode
+from pydantic import BaseModel, Field
 
 dotenv.load_dotenv()
 
@@ -19,7 +20,7 @@ os.environ["LANGCHAIN_TRACING_V2"] = "true" # enable tracing
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 AZURE_INFERENCE_ENDPOINT = os.getenv("AZURE_INFERENCE_ENDPOINT")
 
-# Tools function
+### Tools function
 
 def web_search(query: str) -> str:
     """Search the web for a query.
@@ -36,12 +37,29 @@ def web_search(query: str) -> str:
 tool = [web_search] # create a list of tools
 
 
-# Model from Azure AI Foundry
+### Model from Azure AI Foundry
 model = AzureAIChatCompletionsModel(
     endpoint=AZURE_INFERENCE_ENDPOINT,
     credential=credential,
     model_name='phi-4'
 )
 
-
 llm_with_tools = model.bind_tools(tool)
+
+### Agents
+
+# Search instructions
+prompt_engineer = """
+You are a prompt engineer. You will be given a question and you need format it to be used in a search engine.  
+"""
+
+instrucions = SystemMessage(content=prompt_engineer)
+
+def search_instructions(query: str) -> str:
+    """Format the query to be used in a search engine.
+
+    Args:
+        query: The query to format.
+    """
+    return llm_with_tools([instrucions, HumanMessage(content=query)])
+
